4255679a93ec57481d55aeba692b4e04d5700e5a
tleno <thomas.lenoel@ssi-soft.fr>
2023-08-23 15:37:15 +0200

    Configs workflow for autotesting

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # sfbase
 ![GitHub tag (with filter)](https://img.shields.io/github/v/tag/Calmacil/sfbase)
+![Symfony test](https://github.com/Calmacil/sfbase/actions/workflows/symfony.yml/badge.svg)
 
 Basic symfony website with common features
---
+++ tests/DummyTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+    /**
+     * Simple test to check github capability
+     * @return void
+     */
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
